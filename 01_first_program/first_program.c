c37743fe9fcf9311b5a57a102adf9751f0775f8c
Jose Deleon <jdeleon2317@floridapoly.edu>
2025-01-16 13:30:04 +0000

    Code first program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,22 @@
+// this is the first program in C.
+// It prionts a welcome message to the screen.
+
+// - one-line comment
+/*
+
+This is 
+a multiline comment.
+
+*/
+
+#include <stdio.h>
+
+// Creat a main function
+int main() {
+    // Call the printf function
+    printf("Welcome\nto\nCOP2271!\n");
+    printf("I\nwant\nto\ncode\n");
+
+    return 0;
+    
+    }
