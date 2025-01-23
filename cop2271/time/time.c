4c9902631c913062585443da8ef120064a6895e9
Jose Deleon <jdeleon2317@floridapoly.edu>
2025-01-23 13:37:27 +0000

    integrated hours

--- cop2271/time/time.c
+++ cop2271/time/time.c
@@ -11,6 +11,8 @@
 
     int seconds = 0;
     int minutes = 0;
+    int hours = 0;
+    int remaining_minutes = 0;
     int remaining_seconds = 0;
 
     // Get seconds from input
@@ -18,10 +20,16 @@
     scanf("%d", &seconds);
 
     // Calculate the number of minutes
+
     minutes = seconds / 60;
+    hours = minutes / 60;
+    remaining_minutes = minutes % 60;
     remaining_seconds = seconds % 60;
 
-    printf("%d seconds is %d minutes and %d seconds\n", seconds, minutes, remaining_seconds);
+    printf("%d seconds is %d hours %d minutes and %d seconds\n", seconds, hours, remaining_minutes, remaining_seconds);
+
+    //TODO till thursday
+    //calculate hours *integrated^
 
     return 0;
 
